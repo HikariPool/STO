b8c56e9b42f6c479d47d129e034a87f852855efd
Andrey Medvedev <180141@edu.psuti.ru>
2024-01-31 01:47:41 +0300

    Admin page displaying

--- src/main/resources/static/script/frame/chats.js
+++ src/main/resources/static/script/frame/chats.js
@@ -6,39 +6,28 @@ function reloadContent() {
     itemContainer.innerHTML = '';
 
     $.ajax({
-        url: '/session/get?session_id=' + getParam('session_id'),
+        url: '/chat/getChats',
         type: 'GET',
         success: data => showContentItems(data)
     });
 }
 
-function showContentItems(session) {
-    workspaceHead.setAttribute('session_id', session.id);
-
-    title.html(session.title);
-
-    let contentItems = session.contentItems;
-    for (let i = 0; i < contentItems.length; i++) {
+function showContentItems(chats) {
+    for (let i = 0; i < chats.length; i++) {
         let div = document.createElement('div');
         div.className = 'lineListItem parentElement';
-        div.id = contentItems[i].id;
+        div.id = chats[i].id;
         div.setAttribute('type', 'contentItem');
-
-
-        let img = document.createElement('img');
-        img.className = 'lineListItemImage';
-        img.src = contentItems[i].previewPath;
 
         let h3 = document.createElement('h3');
         h3.className = 'lineListItemTitle';
-        h3.textContent = contentItems[i].title;
+        h3.textContent = chats[i].createdBy.username + ' Message[' + chats[i].messages[0].text + ']';
 
-        div.appendChild(img);
         div.appendChild(h3);
         itemContainer.appendChild(div);
 
-        div.onclick = () =>
-            window.parent.playContent(
-                '/stream/get/' + contentItems[i].sourcePath, undefined, player => window.top.sync(player, session.id), contentItems[i].previewPath);
+//        div.onclick = () =>
+//            window.parent.playContent(
+//                '/stream/get/' + chats[i].sourcePath, undefined, player => window.top.sync(player, chats.id), chats[i].previewPath);
     }
 }
