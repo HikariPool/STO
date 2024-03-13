83ff34f0defe1c88e43e9af79b067a489a1e9c01
Andrey Medvedev <180141@edu.psuti.ru>
2024-03-13 19:40:56 +0300

    Fix shit

--- src/main/resources/static/script/frame/start_frame.js
+++ src/main/resources/static/script/frame/start_frame.js
@@ -2,20 +2,26 @@ let buyGearsButton = document.getElementById('buyGears')
 let serviceButton = document.getElementById('service')
 let inventoryButton = document.getElementById('inventory')
 
-buyGearsButton.addEventListener('click', () => {
-    window.parent.postMessage('/frame/buy_gears_frame.html' )
-});
-
-serviceButton.addEventListener('click', () => {
- $.ajax({
-        url: '/chat/all',
-        type: 'GET',
-        success: data => {
-            window.parent.postMessage('/frame/car_service_frame.html' + '?chat_id=' + data[0].id)
-        }
+if(buyGearsButton != null){
+    buyGearsButton.addEventListener('click', () => {
+        window.parent.postMessage('/frame/buy_gears_frame.html' )
     });
-});
+}
 
-inventoryButton.addEventListener('click', () => {
-    window.parent.postMessage('/frame/inventory_frame.html' )
-});
+if(serviceButton != null){
+    serviceButton.addEventListener('click', () => {
+    $.ajax({
+         url: '/chat/all',
+         type: 'GET',
+         success: data => {
+             window.parent.postMessage('/frame/car_service_frame.html' + '?chat_id=' + data[0].id)
+         }
+     });
+    });
+}
+
+if(inventoryButton != null){
+    inventoryButton.addEventListener('click', () => {
+         window.parent.postMessage('/frame/inventory_frame.html' )
+    });
+}
