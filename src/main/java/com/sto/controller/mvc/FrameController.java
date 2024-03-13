a6134a891b233017b2625597e7c9a096d4d0d0cc
Andrey Medvedev <180141@edu.psuti.ru>
2024-03-13 19:27:11 +0300

    Visibility

--- src/main/java/com/sto/controller/mvc/FrameController.java
+++ src/main/java/com/sto/controller/mvc/FrameController.java
@@ -1,7 +1,10 @@
 package com.sto.controller.mvc;
 
+import com.sto.config.security.service.AccessibilityServiceImpl;
 import com.sto.data.constants.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/frame")
 public class FrameController {
+    @Autowired
+    private AccessibilityServiceImpl accessibilityService;
+
+
     @GetMapping("/{frame}")
-    public String get(@PathVariable String frame) {
+    public String get(@PathVariable String frame, Model model) {
+        model.addAttribute("visibility", accessibilityService.currentUserHasAccess());
         return Constants.FRAME_PATH + frame;
     }
 }
