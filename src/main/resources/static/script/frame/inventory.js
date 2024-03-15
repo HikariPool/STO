3b84a00f096e4025c5f6b492cb3c82e44cfd3eeb
Andrey Medvedev <180141@edu.psuti.ru>
2024-03-15 17:58:31 +0300

    Inventory frontend v1

--- src/main/resources/static/script/frame/inventory.js
+++ src/main/resources/static/script/frame/inventory.js
@@ -1,8 +1,40 @@
 let addButton = document.getElementById('addButton')
 let editButton = document.getElementById('editButton')
+let listItemContainer = document.getElementById('listItemContainer')
 
+let inventoryItemTemplate
+let paramItem
 
+loadTemplates()
 
-function createElement(){
+addButton.addEventListener('click', () => {
+      inventoryItem = createElementFromTemplate(inventoryItemTemplate)
+      paramItem = createElementFromTemplate(paramItem)
+      tbody = inventoryItem.getElementsByTagName('tbody')[0]
 
+      tbody.append(paramItem)
+      listItemContainer.append(inventoryItem)
+});
+
+function createElementFromTemplate(html){
+      const template = document.createElement('template');
+      template.innerHTML = html;
+      return template.content.children[0];
+}
+function loadTemplates(){
+    $.ajax({
+        url: '/frame/inventoryItem.html',
+        type: 'GET',
+        success: (data) => {
+            inventoryItemTemplate = data
+        }
+    });
+
+    $.ajax({
+        url: '/frame/paramItem.html',
+        type: 'GET',
+        success: (data) => {
+            paramItem = data
+        }
+    });
 }
